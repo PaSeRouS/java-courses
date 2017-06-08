7076dee5ff3cdb3ec1b95f4ae3468857940e0dff
Pavel Serov <paser96@mail.ru>
2017-06-09 00:39:33 +0300

    Add sum, difference, multiply, division and pow.

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,25 @@
+import java.lang.Math;
+
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Calculate...");
+
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[1]);
+
+		int sum = first + second;
+		System.out.println("Sum = " + sum);
+
+		int diff = first - second;
+		System.out.println("Diff = " + diff);
+
+		int multiply = first * second;
+		System.out.println("Multiply = " + multiply);
+
+		double div = (double)first / (double)second;
+		System.out.println("Div = " + div);
+
+		double pow = Math.pow(first, second);
+		System.out.println("Pow = " + pow);
 	}
 }
